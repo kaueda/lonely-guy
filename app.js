0ea98abdd48a193db75120d01bcee6cbac638bf9
kaueda <kaue_silveira@hotmail.com>
2013-11-13 00:46:28 -0200

    v0.0.3

--- app.js
+++ app.js
@@ -1,3 +1,86 @@
-function Game() {
-	confirm('Does this work??');
-}
+var wood_amount = 0;
+var stone_amount = 0;
+var food_amount = 0;
+var peasant_amount = 0;
+var appended = false;
+
+//Lembrar de fazer isso!!
+/*var stone_resource_html = "<div id=\"stone\" class=\"resource\">\
+								<span id=\"stone_amount\">You Have: 0 stone pieces</span><br>\
+								<button onclick=\"Game.mineStone()\" id=\"mine\">MINE!</button>\
+							</div>";*/
+
+var Game = {
+	started: false,
+	fps: 10,
+	lastTime: (new Date).getTime(),
+	tickTime: 0,
+	loops: 0,
+	amount: {
+		wood: 0,
+		stone: 0,
+		food: 0,
+		peasant: 1,
+	},
+
+	getDelta: function () {
+		var currTime = (new Date).getTime();
+		var delta = (currTime - Game.lastTime);
+		Game.lastTime = currTime;
+		return delta;
+	},
+
+	huntGame: function() {
+		Game.amount.food++;
+		$('#food_amount').html('You Have: '+Game.amount.food+' meat chunks');
+	},
+
+	chopWood: function() {
+		Game.amount.wood++;
+		$('#wood_amount').html('You Have: '+Game.amount.wood+' logs');
+	},
+
+	mineStone: function() {
+		Game.amount.stone++;
+		$('#stone_amount').html('You Have: '+Game.amount.stone+' stone pieces');
+	},
+
+	update: function () {
+		Game.chopWood();
+	},
+
+	start: function() {
+		if(!Game.started) { 
+			Game._intervalId = setInterval(function () {
+				Game.tickTime += Game.getDelta();
+
+				Game.update();
+				Game.loops++;
+
+				if(Game.tickTime >= 1000) {
+					console.log("FPS: "+Game.loops);
+					Game.loops = 0;
+					Game.tickTime = 0;
+
+					//Other debbuggin logs
+					console.log(Game.amount.food+' meat chunks');
+					console.log(Game.amount.wood+' logs');
+					console.log(Game.amount.stone+' stone pieces');
+					console.log(Game.amount.peasant+' peasants');
+				}
+			}, (1000/Game.fps));
+			Game.started = true;
+		} else {
+			console.log("There is alredy one instance of Game running.");
+		}
+	},
+
+	stop: function() {
+		if(Game.started) {
+			clearInterval(Game._intervalId);
+			Game.started = false;
+		} else {
+			console.log("There is no instance of Game running.");
+		}
+	},
+};
--- utils.js
+++ utils.js
@@ -1,12 +1,19 @@
 var switch_tab;
-var current_tab = "resource";
+var load_tab;
+var current_tab = "messages";
+var div_id_content = $('#content');
 
 $(document).ready(function() {
+	load_tab = function(tab_name) {
+		
+	}
+
 	switch_tab = function(tab_name) {
 		if(tab_name != current_tab) {
 			$("#"+current_tab+"_tab").toggleClass('highlighted');
 			current_tab = tab_name;
 			$("#"+tab_name+"_tab").toggleClass('highlighted');
+			//load_tab(tab_name);
 		}
 	}
 });
